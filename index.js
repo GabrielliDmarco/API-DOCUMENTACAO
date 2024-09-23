0dd17c00d1a758bd7dc903604239b98da795bf7e
GABRIELLI ALANI DE MARCO <gabrielli_marco@estudante.sesisenai.org.br>
2024-09-23 16:02:32 -0300

    Criação da Documentação

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
-
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const rotas_produtos = require('./rotas/rotaProdutos')
 const clientes = require('./rotas/rotaClientes')
-const rotasAutenticacao = require('./rotas/rotaAutenticacao')
+const rotas_autentificacao = require('./rotas/rotaAutenticacao')
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./docs/documentacao.yaml')
 const cookieParser = require('cookie-parser')
 
 app.use(bodyParser.json())
@@ -13,8 +15,11 @@ app.use(cookieParser())
 
 app.use('/produtos', rotas_produtos)
 app.use('/clientes', clientes)
-app.use('/auth', rotasAutenticacao)
+app.use('/auth', rotas_autentificacao)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.listen(5500)
 
-module.exports = app
+
+app.listen(8000)
+
+module.exports = app;
